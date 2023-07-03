c9455c0cf928c281fd109c15aa69885023066231
Roman Böhm <romanboehm@posteo.de>
2023-07-03 17:37:56 +0200

    Remove useless util method overload
    The `com.romanboehm.wichtelnng.utils.MailUtils.findMailFor` version
    returning the list fulfilled the exact same purpose as the one returning
    an `Optional`.

--- src/test/java/com/romanboehm/wichtelnng/utils/MailUtils.java
+++ src/test/java/com/romanboehm/wichtelnng/utils/MailUtils.java
@@ -8,20 +8,10 @@ import jakarta.mail.internet.MimeMessage;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 public class MailUtils {
 
     private MailUtils() {
-    }
-
-    public static Optional<MimeMessage> findMailFor(GreenMailExtension greenMail, String recipient) throws MessagingException {
-        for (var msg : greenMail.getReceivedMessages()) {
-            if (recipient.equals(msg.getRecipients(Message.RecipientType.TO)[0].toString())) {
-                return Optional.of(msg);
-            }
-        }
-        return Optional.empty();
     }
 
     public static List<MimeMessage> findMailFor(GreenMailExtension greenMail, String... recipients) throws MessagingException {
