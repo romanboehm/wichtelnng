4e2fc3fd2bb0aa7767494209808065cecae8fab6
Roman Böhm <romanboehm@posteo.de>
2023-09-30 12:00:57 +0200

    Add doc to ThymeleafLayoutInterceptor
    Before, it used to only live in the accompanying commit message. Which
    was not enough given the rather unusual nature of the code in question.

--- src/main/java/com/romanboehm/wichtelnng/common/web/ThymeleafLayoutInterceptor.java
+++ src/main/java/com/romanboehm/wichtelnng/common/web/ThymeleafLayoutInterceptor.java
@@ -5,6 +5,15 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * Based on <a href="https://blog.codeleak.pl/2013/11/thymeleaf-template-layouts-in-spring.html">Rafał Borowiec's
+ * <em>Thymeleaf template layouts in Spring MVC application with no extensions</em></a>:
+ * <em>"ThymeleafLayoutInterceptor gets the original view name returned from the handler's method and replaces it with
+ * the layout name (that is defined in WEB-INF/views/layouts/default.html). The original view is placed in the model as
+ * a view variable, so it can be used in the layout file. I overrode the postHandle method, as it is executed just
+ * before rendering the view."
+ * </em>
+ */
 public class ThymeleafLayoutInterceptor implements HandlerInterceptor {
 
     private static final String DEFAULT_LAYOUT = "layouts/default";
